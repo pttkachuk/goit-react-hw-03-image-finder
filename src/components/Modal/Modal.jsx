4afe72c85f51968b39a4a46e16ad3c22d3d72af2
pttkachuk <petrotkachuk6@gmail.com>
2023-07-13 20:28:40 +0200

    add modal and styles

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,14 +1,39 @@
-import React, { Component } from 'react'
+
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import style from './Modal.module.css'
 
 export default class Modal extends Component {
-    handleKeyDown = (event) => {
-        if (event.code === 'Escape') {
-            this.props.on
-        }
-    }
+  static propTypes = {
+    toggleModal: PropTypes.func.isRequired,
+    largeImage: PropTypes.string.isRequired,
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    e.code === 'Escape' && this.props.toggleModal();
+  };
+
+  handleBackdropClick = e => {
+    e.target === e.currentTarget && this.props.toggleModal();
+  };
+
   render() {
+    const { handleBackdropClick } = this;
+    const { largeImage } = this.props;
     return (
-      <div>Modal</div>
-    )
+      <div className={style.Overlay} onClick={handleBackdropClick}>
+        <div className={style.Modal}>
+          <img src={largeImage} alt="" />
+        </div>
+      </div>
+    );
   }
 }
